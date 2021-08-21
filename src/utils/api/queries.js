4ecc756cb99e79ca50146a3dbd95d2fcfe39e1af
alanacevedo <michael.acevedo@ug.uchile.cl>
2021-08-21 02:11:58 -0400

    added more queries

--- src/utils/api/queries.js
+++ src/utils/api/queries.js
@@ -1,9 +1,12 @@
-export async function getInfoEstacion(nodeIdString) {
-    const response = await fetch(`http://agua.niclabs.cl/queries/infoestacion?estacion=${nodeIdString}`,
+const apiKey = '919c5e5e086a492398141c1ebd95b711' // Es seguro tener esto aquí?
+
+async function makeRequest(url) {
+    
+    const response = await fetch(url,
     {
         method: 'GET',
         headers: {
-            'query-api-key' : '919c5e5e086a492398141c1ebd95b711',
+            'query-api-key' : apiKey,
             'Content-Type': 'Application/json'
         },
         mode: 'cors',
@@ -11,20 +14,21 @@ export async function getInfoEstacion(nodeIdString) {
     })
 
     return response.json()
+
+}
+
+export async function getInfoEstacion(nodeIdString) {
+    return makeRequest(`http://agua.niclabs.cl/queries/infoestacion?estacion=${nodeIdString}`)  
 }
 
 export async function getAllInfoEstacion() {
-    const response = await fetch(`http://agua.niclabs.cl/queries/infoestacion`,
-    {
-        method: 'GET',
-        headers: {
-            'query-api-key' : '919c5e5e086a492398141c1ebd95b711',
-            'Content-Type': 'Application/json'
-        },
-        mode: 'cors',
-
-    })
-
-    return response.json()
+    return makeRequest(`http://agua.niclabs.cl/queries/infoestacion`)
 }
 
+export async function getEstaciones() {
+    return makeRequest(`http://agua.niclabs.cl/queries/estaciones`)
+}
+
+export async function getDataEstacion(nodeIdString) {
+    return makeRequest(`http://agua.niclabs.cl/queries/dataestaciones?estacion=${nodeIdString}`)  
+}
