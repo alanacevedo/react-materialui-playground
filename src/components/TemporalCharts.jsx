34e4cd68c351590ad005e1dc30f5092abb11bb7e
alanacevedo <michael.acevedo@ug.uchile.cl>
2021-09-30 22:21:48 -0300

    added basic month separation

--- src/components/TemporalCharts.jsx
+++ src/components/TemporalCharts.jsx
@@ -2,18 +2,93 @@ import React, { useContext, useState } from 'react';
 import ActiveNodesContext from '../utils/context/ActiveNodesContext';
 import { Grid } from '@material-ui/core'
 import ToggleHideButton from './ToggleHideButton';
+import useChartCache from '../utils/hooks/useChartCache';
+import { setMonth, setDate, setHours, setMinutes } from 'date-fns'
+import { PlotlyChart } from './PlotlyCharts'
 
 const TemporalCharts = () => {
     const { activeNodes } = useContext(ActiveNodesContext)
     const [shouldHideCharts, setShouldHideCharts] = useState(true)
-
+    const [getCachedNodeData] = useChartCache()
+    
     if (activeNodes.length !== 1) {
         return (<></>)
     } 
 
+    const nodeData = getCachedNodeData(activeNodes[0])
+    const dataArray = nodeData['data']['data']
+    const tempTraces = [];
+
+    const earliestDate = new Date(dataArray[0][0])
+    const earliestMonth = earliestDate.getMonth()
+    const earliestYear = earliestDate.getFullYear()
+
+    // El plan es ir llenando los arreglos y cuando se cambie de mes pushearlo al trace array correspondiente y empezar de nuevo
+    
+    let currentMonth = earliestMonth;
+    let currentYear = earliestYear;
+    let dateArray = [];
+    let tempArray = [];
+
+
+    // #TODO:  idealmente se haría una busqueda (binaria?) y luego copiar slices grandes, no uno a uno.
+    dataArray.forEach((item, index) => {
+
+        /*
+            "headers": [
+            "Date_Time",
+            "Pression [cm H2O]",
+            "Etiqueta P",
+            "Temperatura [°C]",
+            "Etiqueta T",
+            "EC [µs/cm]",
+            "Etiqueta EC"
+        ],
+        */
+
+        const date = new Date(item[0])
+        if (date.getMonth() === currentMonth) {
+            // Se genera una nueva date obviando el año
+            let normalizedDate = new Date()
+            normalizedDate = setDate(normalizedDate, date.getDate())
+            normalizedDate = setHours(normalizedDate, date.getHours())
+            normalizedDate = setMinutes(normalizedDate, date.getMinutes())
+
+            dateArray.push(normalizedDate);
+            tempArray.push(item[3]);
+
+        } else {
+            // Cambio de mes, se genera trace
+
+
+            const tempLineTrace = {
+                x: dateArray,
+                y:  tempArray,
+                mode: 'lines',
+                name: `${currentMonth} ${currentYear}`,
+                line: {
+                    width: 3
+                }
+            };
+
+            tempTraces.push(tempLineTrace);
+            dateArray = [];
+            tempArray = [];
+            currentMonth = date.getMonth();
+            currentYear = date.getFullYear();
+        }
+        // Se salta el último mes, revisar.
+    })
+
+    console.log(tempTraces)
+
+    
+
     const mainComponent = 
     <>
-        hola
+        <Grid item xs={12}>
+            <PlotlyChart chartTraces={tempTraces} dataTag='Temperatura (°C)' threshold={15}/>
+        </Grid>
     </>
 
     return (
